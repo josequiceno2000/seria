0dd8aca41d26c55539b4bd30f264d9c1f46236a9
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-06-07 11:36:45 -0500

    feat(game_data): add game data to play with

--- PROCESS.md
+++ PROCESS.md
@@ -3,3 +3,23 @@
 1. ~~Create virtual environment~~ ✔
 2. ~~Set up project folder structure~~ ✔
 3. ~~Create main.py~~ ✔
+
+## game_data.py
+1. ~~Create game_data.py~~ ✔
+2. ~~Add data (list of dictionaries with name, follower_count, description, and country)~~ ✔
+3. ~~Input real data~~ ✔
+
+## Intro
+0. Create intro.py
+1. Get ASCII title art
+### Intro Function
+1. Create intro function
+2. Display title art 
+3. Display explanation of the game
+4. Let player choose between classic or beat the clock mode
+5. Set up game based on specifications
+6. Return appropriate vale
+7. Connect to main.py
+8. Test function
+### beat_the_clock_or_classic
+1. Create function to ask user to choose mode
---
+++ src/game_data.py
@@ -0,0 +1,272 @@
+data = [
+    {
+        'name': 'Instagram (Official Account)',
+        'follower_count': 690_000_000,
+        'description': 'official Instagram account',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 655_000_000,
+        'description': 'professional footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 505_000_000,
+        'description': 'professional footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 419_000_000,
+        'description': 'singer, actress, and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne "The Rock" Johnson',
+        'follower_count': 393_000_000,
+        'description': 'actor, producer, and former professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 393_000_000,
+        'description': 'media personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'Ariana Grande',
+        'follower_count': 375_000_000,
+        'description': 'singer and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 356_000_000,
+        'description': 'media personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'Beyoncé',
+        'follower_count': 311_000_000,
+        'description': 'singer',
+        'country': 'United States'
+    },
+    {
+        'name': 'Khloé Kardashian',
+        'follower_count': 302_000_000,
+        'description': 'reality television personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'Nike',
+        'follower_count': 300_000_000,
+        'description': 'multinational corporation',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Bieber',
+        'follower_count': 294_000_000,
+        'description': 'pop singer and songwriter',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Kendall Jenner',
+        'follower_count': 287_000_000,
+        'description': 'fashion model and television personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'Taylor Swift',
+        'follower_count': 281_000_000,
+        'description': 'singer-songwriter',
+        'country': 'United States'
+    },
+    {
+        'name': 'National Geographic',
+        'follower_count': 278_000_000,
+        'description': 'renowned magazine',
+        'country': 'United States'
+    },
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 273_000_000,
+        'description': 'international cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Jennifer Lopez',
+        'follower_count': 248_000_000,
+        'description': 'singer, actress, and dancer',
+        'country': 'United States'
+    },
+    {
+        'name': 'Neymar Jr.',
+        'follower_count': 229_000_000,
+        'description': 'professional footballer',
+        'country': 'Brazil'
+    },
+    {
+        'name': 'Nicki Minaj',
+        'follower_count': 225_000_000,
+        'description': 'rapper, singer, and songwriter',
+        'country': 'Trinidad and Tobago'
+    },
+    {
+        'name': 'Kourtney Kardashian',
+        'follower_count': 218_000_000,
+        'description': 'reality television personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'Miley Cyrus',
+        'follower_count': 212_000_000,
+        'description': 'singer, songwriter, and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Katy Perry',
+        'follower_count': 204_000_000,
+        'description': 'singer',
+        'country': 'United States'
+    },
+    {
+        'name': 'Zendaya',
+        'follower_count': 178_000_000,
+        'description': 'actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kevin Hart',
+        'follower_count': 177_000_000,
+        'description': 'comedian',
+        'country': 'United States'
+    },
+    {
+        'name': 'Real Madrid C.F.',
+        'follower_count': 176_000_000,
+        'description': 'professional football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'Cardi B',
+        'follower_count': 163_000_000,
+        'description': 'rapper',
+        'country': 'United States'
+    },
+    {
+        'name': 'LeBron James',
+        'follower_count': 159_000_000,
+        'description': 'basketball player',
+        'country': 'United States'
+    },
+    {
+        'name': 'Chris Brown',
+        'follower_count': 144_000_000,
+        'description': 'singer, songwriter, and dancer',
+        'country': 'United States'
+    },
+    {
+        'name': 'NASA',
+        'follower_count': 98_000_000,
+        'description': 'scientific body',
+        'country': 'United States'
+    },
+    {
+        'name': 'FC Barcelona',
+        'follower_count': 142_000_000,
+        'description': 'professional football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'Shaquille O\'Neal',
+        'follower_count': 35_000_000,
+        'description': 'basketball player',
+        'country': 'United States'
+    },
+    {
+        'name': 'Gordon Ramsay',
+        'follower_count': 18_000_000,
+        'description': 'celebrity chef',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Netflix',
+        'follower_count': 300_000_000,
+        'description': 'streaming service',
+        'country': 'United States'
+    },
+    {
+        'name': 'Humans of New York',
+        'follower_count': 18_000_000,
+        'description': 'popular photoblog ',
+        'country': 'United States'
+    },
+    {
+        'name': 'MrBeast',
+        'follower_count': 74_000_000,
+        'description': 'YouTuber',
+        'country': 'United States'
+    },
+    {
+        'name': 'Discovery',
+        'follower_count': 60_000_000,
+        'description': 'wildlife, science, and adventure content.',
+        'country': 'United States'
+    },
+    {
+        'name': 'Priyanka Chopra Jonas',
+        'follower_count': 93_000_000,
+        'description': 'actress, singer, and film producer',
+        'country': 'India'
+    },
+    {
+        'name': 'Charli D\'Amelio',
+        'follower_count': 49_000_000,
+        'description': 'social media personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'Marvel Studios',
+        'follower_count': 72_000_000,
+        'description': 'comic book universe',
+        'country': 'United States'
+    },
+    {
+        'name': 'Tasty (BuzzFeed Food)',
+        'follower_count': 42_000_000,
+        'description': 'food network',
+        'country': 'United States'
+    },
+    {
+        'name': 'BBC News',
+        'follower_count': 23_000_000,
+        'description': 'news network',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'The Louvre Museum',
+        'follower_count': 6_000_000,
+        'description': 'art museum',
+        'country': 'France'
+    },
+    {
+        'name': 'UNICEF',
+        'follower_count': 10_000_000,
+        'description': 'charity',
+        'country': 'United States'
+    },
+    {
+        'name': 'The New York Times',
+        'follower_count': 19_000_000,
+        'description': 'newspaper',
+        'country': 'United States'
+    },
+    {
+        'name': 'Red Bull',
+        'follower_count': 19_000_000,
+        'description': 'sports drink',
+        'country': 'Austria'
+    },
+]
